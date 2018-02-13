d9235f05926d96ca9d39f815df3a9c9529927bf8
Sharmina Akhtar <sakht318@gmail.com>
2018-02-13 17:57:59 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,9 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "Select SUM(pledges.amount), projects.title From projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id
-  Where projects.funding_goal = pledges.amount;"
+  Where projects.funding_goal = pledges.amount
+  GROUP BY projects.funding_goal
+  HAVING SUM(pledges.amount);"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
